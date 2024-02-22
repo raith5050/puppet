3b7b184c594f8e5c6258a4ebc1344db93910decc
Cody Smith <codysmith0704@gmail.com>
2024-02-22 13:06:14 -0500

    adding a file copy in test-text

--- manifests/test-text.pp
+++ manifests/test-text.pp
@@ -12,3 +12,7 @@ file { '/tmp/hello3.txt':
   ensure => file,
   content => "testing yet another push!",
 }
+
+file {'/tmp/MOTD.txt':
+  source => 'https://github.com/bitfield/puppet-beginners-guide-3/blob/571fe017716763f9dab3fff09d809d50c7bb133b/examples/files/motd.txt'
+}
