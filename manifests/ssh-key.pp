38614c89099630214027e8a8aa506f217bd9573d
Cody Smith <codysmith0704@gmail.com>
2024-02-22 19:46:36 -0500

    corrected

--- manifests/ssh-key.pp
+++ manifests/ssh-key.pp
@@ -1,10 +1,3 @@
-ssh_authorized_key { 'blizzard\codsmith@IR-CODSMITH2-WL':
-  ensure => absent
-  user   => 'root',
-  type   => 'ssh-rsa',
-  key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABgQCpdP+O3mYW4+NGZR8IJ2qrWgevkP9m/bZVya+C2d5T6kDdxSyvrXoGv8orPeIjuWnA7a6ezSIL9ywuA0SUWBslvPumt8BHmei/vNsupXWhUS9PIVMNrkCIPlrDBsTxCDRu/pPXLXdz66R+OyFARlRvzhni7w79ASb4yV6uAzbMOqL/mPQZ0SQpt8H6mou+agF8sj3oZX82JefiMKHqGwhbqDs4nCtvNxUYC6YFWra+xuFOPx6NXdN8oFJHNDmkW11h/wBtMKR9GMfBi+HPSYJZ7Qb4OUru8QewB0njAYtdJhkj96OyyUId6o7eUi1J2W31vHs1eDjV9yJ6TxBP8Kbmp+g3nl1XK9sySkx6dwa4Sm8Fk5Vtv99ti0FrxlSTMnZ5Owc2J9aJs+b3SLd5Qftgdyjww3Wgx5wYKNOHJZqosbCxQ9ynIb9ZNZxoiwVaSz7vEvgYQomjrhP82en8C9XufVPjKzgiBXQf0roSjbp0tPEn7Zx4sfWK4WGEEKPoLxU=',
-}
-
 ssh_authorized_key { 'codsmith':
   user   => 'root',
   type   => 'ssh-rsa',
