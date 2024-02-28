0d70074e67cab0f3700b4c81a9c7fccb9bdf6f6c
Cody Smith <codysmith0704@gmail.com>
2024-02-28 15:50:32 -0500

    added facter statements

--- manifests/fact-hash.pp
+++ manifests/fact-hash.pp
@@ -1,1 +1,11 @@
 notice($facts['os']['architecture'])
+
+if $facts['os']['selinux']['enabled'] {
+  notice('SElinux is enabled')
+} else {
+  notice('SElinux is disabled')
+}
+
+notice("My hostname is ${facts['hostname']}")
+notice("My FQDN is ${facts['fqdn']}")
+notice("My IP is ${facts['networking']['ip']}")
