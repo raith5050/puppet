7ab4433877b885d5ef1c0afbb80ff0012b394f94
Cody Smith <codysmith0704@gmail.com>
2024-02-28 16:58:14 -0500

    iteration improved

--- manifests/iteration_simple.pp
+++ manifests/iteration_simple.pp
@@ -12,3 +12,17 @@ file { '/usr/local/bin/task3':
   content => "echo I am task3\n",
   mode    => '0755',
 }
+
+$tasks = ['task4','task5','task6']
+$tasks.each | $task | {
+  file {"/usr/local/bin/${task}":
+    content => "echo I am ${task}\n",
+    mode    => '0755',
+  }
+}
+
+$nics = $facts['networking]['interfaces']
+
+$nics.each | String $interface, Hash $attributes | {
+  notice("interface ${interface} has IP ${attributes['ip']}")
+}
