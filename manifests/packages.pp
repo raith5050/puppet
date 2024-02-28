3835eebddec9260bccb7389dc1f003bacedfb360
Cody Smith <codysmith0704@gmail.com>
2024-02-28 14:53:33 -0500

    added if to packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,12 +3,10 @@ $install_cowsay = false
 
 if $install_cowsay {
   package { 'cowsay':
-    notice('Installing cowsay'),
     ensure => installed,
   }
 } else {
   package { 'cowsay':
-    notice('Uninstalling cowsay'),
     ensure => absent,
   }
 }
